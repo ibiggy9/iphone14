4c08f1a74ef9069c4fcbc8b59917739769be3354
Main <main@Mains-MacBook-Pro.local>
2022-09-13 20:37:51 -0400

    hi

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 import smtplib
 from email.message import EmailMessage
 import traceback
@@ -28,13 +29,13 @@ def scrape():
     try:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        chrome_options.add_argument("--disable-dev-shm-usage")
+        #chrome_options.add_argument("--disable-dev-shm-usage")
         #chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--window-size=400x1500")
+        #chrome_options.add_argument("--window-size=400x1500")
         chrome_options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36')
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        driver = webdriver.Chrome(service=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
         
         url = "https://www.apple.com/ca/shop/buy-iphone/iphone-14-pro/6.7-inch-display-128gb-space-black"
         driver.get(url)
